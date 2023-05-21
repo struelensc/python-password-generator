7e3d0565325f5c42a84f0698aae1c7b7449b45c6
Courtney Struelens <courtneystruelens@hotmail.com>
2023-05-20 17:10:43 -0700

    added in random character generation and stitching password together

--- main.py
+++ main.py
@@ -6,7 +6,16 @@ def shuffle(string):
   random.shuffle(tempList)
   return ''.join(tempList)
 
-password = None # combine random chars
+uppercaseLetter1=chr(random.randint(65,90)) #generate random uppercase letter
+uppercaseLetter2=chr(random.randint(65,90)) #generate random uppercase letter
+lowercaseLetter1=chr(random.randint(97,122)) #generate random lowercase letter
+lowercaseLetter2=chr(random.randint(97,122)) #generate random lowercase letter
+number1=chr(random.randint(48,57)) #generate random number
+number2=chr(random.randint(48,57)) #generate random number
+special1=chr(random.randint(33,64)) #generate random special character
+special2=chr(random.randint(33,64)) #generate random special character
+
+password = uppercaseLetter1 + uppercaseLetter2 + lowercaseLetter1 + lowercaseLetter2 + number1 + number2 + special1 + special2
 password = shuffle(password)
 
 #ouput
